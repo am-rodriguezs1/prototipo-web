c409453e38f2e762f5d7027dc46512110f43ae71
Juan Camilo Acevedo Ospina <juancamilo@MacBook-Pro-de-Juan.local>
2026-03-10 22:05:46 -0500

    feat: screen massive confirmation

---
+++ src/app/features/alarmas/importacion-masiva/steps/step3-confirmacion/step3-confirmacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-step3-confirmacion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './step3-confirmacion.component.html',
+  styleUrls: ['./step3-confirmacion.component.scss']
+})
+export class Step3ConfirmacionComponent {}
